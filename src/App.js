ad2daef64f47db2c1bed1d4f1da21eb01ea92d6c
TimothyBlajsa <tbla3900@stu.raritanval.edu>
2022-03-02 16:12:20 -0500

    Create app.js file
    Create card.scss file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+import style from './scss/card.scss'
+
+const App = () => {
+return (
+    <div className='App'>
+        <div className='Card'>Hello</div>
+    </div>
+)
+}
+
+export default App;
